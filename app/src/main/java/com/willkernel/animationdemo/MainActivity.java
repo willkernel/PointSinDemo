3b141509344fd0977a471ddc5536b0f11531a01d
willkernel <willkerneljc@gmail.com>
2019-03-06 23:19:07 +0800

    xml animator

--- app/src/main/java/com/willkernel/animationdemo/MainActivity.java
+++ app/src/main/java/com/willkernel/animationdemo/MainActivity.java
@@ -1,15 +1,19 @@
 package com.willkernel.animationdemo;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        PointSinView pointSinView=findViewById(R.id.pointView);
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.property_animator);
+        set.setTarget(pointSinView);
+        set.start();
 
     }
 }
